b31123f12a0700c089aaaa24644e540baab99645
Mango Juice <123135130+MangoJoose@users.noreply.github.com>
2023-09-30 19:13:27 +0000

    IAMSODONEWITHTHISSHIT

---
+++ service/src/infrastructure/db/migrations/20230930191310_product_array/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `rating` on the `Product` table. All the data in the column will be lost.
+  - You are about to drop the column `review` on the `Product` table. All the data in the column will be lost.
+
+*/
+-- CreateTable
+CREATE TABLE "Review" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "review" TEXT NOT NULL DEFAULT 'Test',
+    "rating" INTEGER NOT NULL DEFAULT 0
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Product" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "price" REAL NOT NULL,
+    "imageUrl" TEXT,
+    "reviewId" INTEGER NOT NULL DEFAULT 0,
+    CONSTRAINT "Product_reviewId_fkey" FOREIGN KEY ("reviewId") REFERENCES "Review" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Product" ("description", "id", "imageUrl", "price", "title") SELECT "description", "id", "imageUrl", "price", "title" FROM "Product";
+DROP TABLE "Product";
+ALTER TABLE "new_Product" RENAME TO "Product";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
